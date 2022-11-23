50a57201cc1af7b85f1631ec4c9c0c7c0cebae53
Sunbearian <stigcollins@icloud.com>
2022-11-23 15:54:15 +0000

    Added cors to the routes, server

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import logger from "morgan";
 import cardRouter from "./routes/routes.js";
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
