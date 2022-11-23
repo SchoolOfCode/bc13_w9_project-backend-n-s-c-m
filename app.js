e817c1c3569a7eb6e6be512beaab68247fe61bc5
Natasha <natasha.chinea@gmail.com>
2022-11-23 14:00:25 +0000

    router added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import logger from "morgan";
-
+import cardRouter from "./routes/routes";
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use("/api/cards", cardRouter);
 
 export default app;
---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+import express  from "express";
+import { getCards, createCards } from "../models";
+
+const cardRouter = express.Router();
+
+//PLAN: 
+//import function from models (charles)
+//set up get function that handles the get request
+//set up a POST function that handles post request
+
+cardRouter.get("/", async (req, res)=> {
+    const cards = await getCards()
+    res.status(200).json({ success: true, payload: cards })
+})
+
+cardRouter.post("/", async (req, res)=> {
+    const newCard = req.body
+    const result = await createCards(newCard)
+    res.status(201).json({ success: true, payload: result })
+})
+
+
+
+export default cardRouter;
