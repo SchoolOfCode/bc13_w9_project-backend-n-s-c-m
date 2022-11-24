4ee9604c700be37a3e6b5c2bb4ed9f6187cc8270
Sunbearian <stigcollins@icloud.com>
2022-11-24 16:12:32 +0000

    Added delete routes and model

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getCards, createCards } from "../models/index.js";
+import { getCards, createCards, deleteCard } from "../models/index.js";
 import cors from "cors";
 
 const cardRouter = express.Router();
@@ -9,16 +9,21 @@ const cardRouter = express.Router();
 //set up get function that handles the get request
 //set up a POST function that handles post request
 
-
-cardRouter.get("/", async (req, res)=> {
-    const cards = await getCards()
-    res.status(200).json({ success: true, payload: cards })
-})
+cardRouter.get("/", async (req, res) => {
+	const cards = await getCards();
+	res.status(200).json({ success: true, payload: cards });
+});
 
 cardRouter.post("/", async (req, res) => {
 	const newCard = req.body;
 	const result = await createCards(newCard);
 	res.status(201).json({ success: true, payload: result });
+});
+
+cardRouter.delete("/:id", async (req, res) => {
+	const id = req.params.id;
+	const result = await deleteCard(id);
+	res.status(200).json({ success: true, payload: result });
 });
 
 export default cardRouter;
